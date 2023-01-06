f48002ec5e745ef3a2dc67422263ac301d6967ad
aqsa.rasheed <aqsa.rasheed@CS-DT-BASIT>
2023-01-06 03:35:22 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using CustomLinkedList;
+namespace MyLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomLinkedList.LinkedList<string> ll = new CustomLinkedList.LinkedList<string>();
+
+            Node<string> a = new Node<string>("aaa");
+            ll.AddFirst(a);
+
+            Node<string> b = new Node<string>("bbb");
+            ll.AddFirst(b);
+
+            Node<string> c= new Node<string>("ccc");
+            ll.AddFirst(c);
+
+            Node<string> d = new Node<string>("ddd");
+            ll.AddFirst(d);
+
+            ll.Traverse();
+            Console.WriteLine("Add after a ");
+            ll.AddAfter(new Node<string>("xxx"), a);
+            ll.Traverse();
+
+            Node<string> target = ll.Find("ddd");
+            if (target != null)
+            {
+                Console.WriteLine("found: " + target.Data);
+            }
+            else
+            {
+                Console.WriteLine("foobar");
+            }
+
+            Console.WriteLine("\nremoving " + target.Data);
+            ll.Remove(target);
+            ll.Traverse();
+        }
+    }
+}
